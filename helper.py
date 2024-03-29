8ed70f5718bc7b4c13994c553b104c92f00cac86
johny <johny@sins.com>
2024-03-29 21:22:18 +0530

    feat fix

--- helper.py
+++ helper.py
@@ -1,4 +1,4 @@
-import os
+import subprocess
 
 def get_cmd(name,number):
     return f"Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/coderuster/cnlabprgm/main/{number}/{name}' -OutFile '{name}'"
@@ -7,8 +7,8 @@ def main():
     number=input("Enter the experiment number: ").strip()
     cmd1=get_cmd("server.py",number)
     cmd2=get_cmd("client.py",number)
-    os.system(cmd1)
-    os.system(cmd2)
+    subprocess.run(cmd1.split())
+    subprocess.run(cmd2.split())
 
 if __name__=="__main__":
     main()
